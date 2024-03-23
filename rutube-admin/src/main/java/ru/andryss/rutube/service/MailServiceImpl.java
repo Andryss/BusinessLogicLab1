4cd9d7283f7ea88f5982ec47afa02a81e1eac68c
Andryss <andr.krivosheev@gmail.com>
2024-03-23 11:11:52 +0300

    Add readable messages

--- rutube-admin/src/main/java/ru/andryss/rutube/service/MailServiceImpl.java
+++ rutube-admin/src/main/java/ru/andryss/rutube/service/MailServiceImpl.java
@@ -27,11 +27,16 @@ public class MailServiceImpl implements MailService {
             messageHelper.setTo(email);
             messageHelper.setFrom("rutube-parody-admin@yandex.ru");
             messageHelper.setSubject("Moderation pending action");
-            messageHelper.setText(sourceId);
+            messageHelper.setText(formatMessage(moderator, sourceId));
 
             mailSender.send(message);
         } catch (MessagingException e) {
             log.error("error occurred while sending moderation pending notification", e);
         }
+    }
+
+    private String formatMessage(String moderator, String sourceId) {
+        return "Dear " + moderator + ". You have assigned to moderate source with ID " + sourceId +
+                ". Please moderate it as fast as possible!";
     }
 }
--- rutube/src/main/java/ru/andryss/rutube/service/MailServiceImpl.java
+++ rutube/src/main/java/ru/andryss/rutube/service/MailServiceImpl.java
@@ -30,11 +30,30 @@ public class MailServiceImpl implements MailService {
             messageHelper.setTo(email);
             messageHelper.setFrom("rutube-parody@yandex.ru");
             messageHelper.setSubject("Videos pending actions");
-            messageHelper.setText(counts.toString());
+            messageHelper.setText(formatMessage(author, counts));
 
             mailSender.send(message);
         } catch (MessagingException e) {
             log.error("error occurred while sending videos pending actions notification", e);
         }
+    }
+
+    private String formatMessage(String author, Map<VideoStatus, Long> counts) {
+        StringBuilder builder = new StringBuilder("Dear ").append(author).append(". You have");
+        Long notUploaded = counts.get(VideoStatus.UPLOAD_PENDING);
+        Long notFilled = counts.get(VideoStatus.FILL_PENDING);
+        if (notUploaded != null && notUploaded > 0) {
+            builder.append(' ').append(notUploaded.longValue()).append(" videos pending uploading");
+            if (notFilled != null && notFilled > 0) {
+                builder.append(" and ").append(notFilled.longValue()).append(" videos pending filling");
+            }
+            builder.append(". Please take actions!");
+        } else if (notFilled != null && notFilled > 0) {
+            builder.append(' ').append(notFilled.longValue()).append(" videos pending filling").append(". Please take actions!");
+        } else {
+            log.warn("Sending notification with empty counts to {}", author);
+            builder.append(" no unpublished videos to take care. Relax :)");
+        }
+        return builder.toString();
     }
 }
